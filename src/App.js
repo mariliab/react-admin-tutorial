6545ce63cf2a41aad1085a0368cf1ad83120b819
mariliab <marilia.bognandi@gmail.com>
2021-09-29 13:36:08 +0200

    add dashboard as startpage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import * as React from "react";
 import { Admin, Resource } from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
+
 import { UserList } from "./users";
 import { PostList, PostEdit, PostCreate } from "./posts";
-import jsonServerProvider from "ra-data-json-server";
+import { Dashboard } from "./dashboard";
 import PostIcon from "@material-ui/icons/Book";
 import UserIcon from "@material-ui/icons/Group";
 
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 const App = () => (
-  <Admin dataProvider={dataProvider}>
+  <Admin dataProvider={dataProvider} dashboard={Dashboard}>
     <Resource name="users" list={UserList} icon={UserIcon} />
     <Resource
       name="posts"
